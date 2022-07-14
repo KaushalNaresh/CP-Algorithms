213f8bd0ee59bc431776c73a6d0d5bbbcdb94e20
Naresh Kumar Kaushal <kaushalnaresh2689@gmail.com>
2022-07-15 02:16:00 +0530

    Add files via upload

---
+++ lowest_common_ancestor.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    /*
+        Method 1 : Using recursion and keeping track of whether p and q are visited
+        Time Complexity : O(N) where N is number of nodes in a tree
+        Space Complexity : O(H) where H is the height os a tree
+    */
+    
+    pair<int, TreeNode*> utilFunc(TreeNode* root, TreeNode* p, TreeNode* q){
+        
+        if(root == nullptr)
+            return {0, nullptr};
+        
+        pair<int, TreeNode*> ls = utilFunc(root->left, p, q);
+        pair<int, TreeNode*> rs = utilFunc(root->right, p, q);
+        
+        if(ls.first == 2 || rs.first == 2) // If already ancestor is found
+            return ls.first == 2 ? pair<int, TreeNode*>{2, ls.second} : pair<int, TreeNode*>{2, rs.second};
+        
+        int k = ls.first+rs.first;
+        TreeNode* parentNode = nullptr;
+        if(root->val == p->val || root->val == q->val)
+            k += 1;
+        
+        return k != 2 ? pair<int, TreeNode*>{k, nullptr} : pair<int, TreeNode*>{k, root};
+        
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        return utilFunc(root, p, q).second;
+    }
+    
+    /*
+        Method 2 : Using recursion and BST properties
+        Time Complexity : O(H) where H is the height of a tree
+        Space somplexity : O(1)
+    */
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        while((root->val-p->val) * (root->val-q->val) > 0)
+            root = p->val > root->val ? root->right : root->left;
+        
+        return root;
+    }
+};
